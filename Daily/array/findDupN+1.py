0bbabf16279086845ae094ecfecf4ace63e53cda
mrinalinichava <mrinalinichava1@gmail.com>
2024-07-10 22:40:16 -0400

    find duplicate in an array of size n+1 , optimized

--- Daily/array/findDupN+1.py
+++ Daily/array/findDupN+1.py
@@ -6,5 +6,24 @@ def findDuplicate(arr):
             return arr[i]
     return -1
 
-arr = [1,2,3,4,5,5]
+def findDuplicateOptimized(arr):
+    slow = arr[0]
+    fast = arr[0]
+
+    while(True):
+        slow = arr[slow]
+        fast = arr[arr[fast]]
+        if(slow==fast):
+            break
+    slow = arr[0]
+    while slow != fast:
+        slow = arr[slow]
+        fast = arr[fast]
+
+    return slow
+
+
+
+arr = [1,2,3,4,6,5,6]
+print(findDuplicate(arr))
 print(findDuplicate(arr))
