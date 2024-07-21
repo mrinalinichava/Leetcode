76170ff51b078910b35e67a9beda9e7028f2a827
mrinalinichava <mrinalinichava1@gmail.com>
2024-07-20 22:36:31 -0400

    majority element in an array (i.e which appears more than n/2 times) - using moores voting algorithm

--- Daily/array/majorityElement.py
+++ Daily/array/majorityElement.py
@@ -10,11 +10,30 @@ def majorityElement(arr):
     return -1
 
 
+# Moores voting algorithm
+
+def majorityElementOptimal(arr):
+    count = 0
+    majorityelement = None
+    for ele in arr:
+        if(count == 0):
+            majorityelement = ele
+
+        else:
+            if(majorityelement == ele):
+                count+=1
+            else:
+                count-=1
+    return majorityelement
+
+
 
 
 arr = [3,1,3]
 arr1 = [2,2,1,1,1,2,2]
 arr2 = [2,1,1,1,2,2]
+arr3 = [7,7,5,7,5,1,5,7,5,7]
 print(majorityElement(arr))
 print(majorityElement(arr1))
 print(majorityElement(arr2))
+print(majorityElementOptimal(arr3))
