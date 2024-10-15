a4371222f6cf3e172d5c37c6674f584c0efd980b
mrinalinichava <mrinalinichava1@gmail.com>
2024-10-14 23:46:50 -0400

    roman to integer

---
+++ Daily/math/romanToInteger.py
@@ -0,0 +1,27 @@
+def romanToInteger(s):
+    d= {
+        'I': 1,
+        'V':5,
+        'X': 10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000
+    }
+
+    total = 0
+    for i,ele in enumerate(s):
+
+        if(i<len(s)-1 and d[s[i]]<d[s[i+1]]):
+            total -= d[s[i]]
+
+        else:
+            total += d[s[i]]
+    return total
+
+
+number = 'XLVII'
+num1 = 'XVII'
+
+print(romanToInteger(number))
+print(romanToInteger(num1))
